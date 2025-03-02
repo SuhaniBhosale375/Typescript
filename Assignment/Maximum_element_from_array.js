2a643d968df6b1b887e6650502d2ff624fc645b4
SuhaniBhosale375 <suhanibhosale375@gmail.com>
2025-03-02 14:00:22 +0530

    Typescript Program

---
+++ Assignment/Maximum_element_from_array.js
@@ -0,0 +1,12 @@
+var Arr = [23, 89, 6, 29, 56, 45, 77, 32];
+function Maximum() {
+    var i = 0;
+    var Max = 0;
+    for (i = 0; i < Arr.length; i++) {
+        if (Arr[i] > Max) {
+            Max = Arr[i];
+        }
+    }
+    console.log("Maximum element is :" + Max);
+}
+Maximum();
